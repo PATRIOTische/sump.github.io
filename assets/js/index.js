07bcc91b93d2675028d615ba7b1b96151290358f
+PATRIOTische <108129770+PATRIOTische@users.noreply.github.com>
2024-04-08 13:20:09 +0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+var isMaintenanceActive = true;
+var redirectDelay = 1000;
+
+document.addEventListener("DOMContentLoaded", function () {
+    var maintenanceText = document.getElementById("maintenanceText");
+    if (isMaintenanceActive) {
+        document.cookie = "maintenance=true";
+    } else {
+        maintenanceText.style.display = "none";
+        setTimeout(function () {
+            window.location.href = "MAIN-СУМП.html";
+        }, redirectDelay);
+    }
+});
+
+function reloadButton() {
+    location.reload();    
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var maintenanceMessage = document.getElementById("maintenanceMessage");
+    if (isMaintenanceActive) {
+        maintenanceMessage.style.display = "block";
+    }
+});
+
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+  
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
