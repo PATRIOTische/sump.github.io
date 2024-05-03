7ec639fb9484332f9fe2a5711483e34e262e94d0
+PATRIOTische <108129770+PATRIOTische@users.noreply.github.com>
2024-05-03 22:33:55 +0300

    Add files via upload

---
+++ assets/js/auto.js
@@ -0,0 +1,19 @@
+document.body.style.overflowY = 'auto';
+
+function redirectToDiscord() {
+
+  window.location.href = 'discord(link).html';
+
+}
+
+function redirectToTikTok() {
+
+  window.location.href = 'https://www.tiktok.com/@sumptranslate.official';
+
+}
+
+function redirectToYoutube() {
+
+  window.location.href = 'https://www.youtube.com/@sumptranslate';
+
+}
