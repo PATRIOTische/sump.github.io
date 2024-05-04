faf488b1710f6ad7b5e1ae62ff8a90ed2b4f90df
+PATRIOTische <108129770+PATRIOTische@users.noreply.github.com>
2024-05-04 21:16:30 +0300

    Delete assets/js/404.js

--- assets/js/404.js
+++
@@ -1,9 +0,0 @@
-const backgroundImages = [
-    'assets/svg/--bg--theme-dark1.svg',
-    'assets/svg/--bg--theme-dark2.svg',
-    'assets/svg/--bg--theme-dark3.svg',
-    'assets/svg/--bg--theme-dark4.svg'
-];
-
-const randomIndex = Math.floor(Math.random() * backgroundImages.length);
-document.body.style.backgroundImage = `url(${backgroundImages[randomIndex]})`;
